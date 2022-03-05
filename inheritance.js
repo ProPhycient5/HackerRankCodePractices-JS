1d0d7d8adbb0ade64a306df3c56771a567adbdb4
Sawan Kuma Das <71059909+ProPhycient5@users.noreply.github.com>
2022-03-06 00:44:23 +0530

    inheritance problem.
    One of the fundamental concepts but quite unknown concept to the most web developers

---
+++ inheritance.js
@@ -0,0 +1,36 @@
+// Question: https://www.hackerrank.com/challenges/js10-inheritance/problem?isFullScreen=true
+
+// Solution:
+
+class Rectangle {
+    constructor(w, h) {
+        this.w = w;
+        this.h = h;
+    }
+}
+// ******
+
+Rectangle.prototype.area = function() {
+ return this.w * this.h;
+}
+
+class Square extends Rectangle{
+    constructor(w){
+        super(w);
+        this.h = w;
+    }
+}
+
+// *******
+
+
+if (JSON.stringify(Object.getOwnPropertyNames(Square.prototype)) === JSON.stringify([ 'constructor' ])) {
+    const rec = new Rectangle(3, 4);
+    const sqr = new Square(3);
+    
+    console.log(rec.area());
+    console.log(sqr.area());
+} else {
+    console.log(-1);
+    console.log(-1);
+}
